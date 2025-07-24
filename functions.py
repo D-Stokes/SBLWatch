9ec32ffd780a94f6f54e170907b5c05f31b3ef86
dan_s <dstokesart@gmail.com>
2025-07-24 15:57:01 +0100

    separate functions and variables from main file

--- functions.py
+++ functions.py
@@ -9,3 +9,14 @@ def get_channel_videos(api_key, uploads_playlist_id, max_results=50):
 def get_video_stats(api_key, video_id):
     url = f"https://www.googleapis.com/youtube/v3/videos?part=statistics,snippet&id={video_id}&key={api_key}"
     return requests.get(url).json()
+
+
+# flatten the json output of the api so i can put it in a spreadsheet
+def flatten_video_metadata(video_item):
+    flat = {}
+    for section in ["snippet", "contentDetails", "statistics"]:
+        data = video_item.get(section, {})
+        for key, value in data.items():
+            flat[f"{section}_{key}"] = value
+
+    return flat
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import requests
 import pandas as pd
 import isodate
-from functions import get_channel_videos, get_video_stats
+from datetime import date, timedelta
 from video_ids import VIDEO_ID
 
 """
@@ -17,60 +17,51 @@ api_key = os.getenv("YOUTUBE_API_KEY")
 handle = "devinebass"
 url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&q={handle}&key={api_key}"
 
+# set dates and empty list object
+today = date.today().strftime("%Y-%m-%d")
+yesterday = (date.today() - timedelta(1)).strftime("%Y-%m-%d")
+yesterday_csv_path = f'output/video_data{yesterday}.csv'
+today_csv_path = f'output/video_data{today}.csv'
+new_rows = []
+
 response = requests.get(url).json()
 channel_id = response["items"][0]["snippet"]["channelId"]
 
 print("Channel ID:", channel_id)
-uploads_playlist_id = "UU" + channel_id[2:]
-print(uploads_playlist_id)
 
-results = get_channel_videos(api_key, uploads_playlist_id)
-print(results)
+"""
+main loop, iterates through list of video ids, pulls all metadata and 
+inserts it in to a csv file
+"""
+for video_id in VIDEO_ID:
+    print("Video ID:", video_id)
+    url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id={video_id}&key={api_key}"
 
-video_data = []
+    response = requests.get(url)
+    data = response.json()
 
-for item in results:
-    snippet = item['snippet']
-    content = item['contentDetails']
+    # Parse the stats
+    if data:
+        stats = data["items"][0]["statistics"]
+        content = data["items"][0]["contentDetails"]
+        snip = data["items"][0]["snippet"]
+        dur_secs = int(isodate.parse_duration(content["duration"]).total_seconds())
+        flat_data = {'video_id':video_id, 'date':date.today().isoformat(), 'dur_secs':dur_secs} | stats | content | snip
+        new_rows.append(flat_data)
+    else:
+        print("Video not found or API key error.")
 
-    video_data.append({
-        'title': snippet['title'],
-        'video_id': content['videoId'],
-        'publish_date': snippet['publishedAt'],
-        'channel': snippet['channelTitle'],
-        'description': snippet['description'],
-        'thumbnail_url': snippet['thumbnails'].get('high', {}).get('url'),
-        'video_url': f"https://www.youtube.com/watch?v={content['videoId']}"
-    })
+append_df = pd.DataFrame(new_rows) # convert to dataframe
 
-# Create DataFrame
-df = pd.DataFrame(video_data)
-
-# Optional: Convert publish date to datetime
-df['publish_date'] = pd.to_datetime(df['publish_date'])
-
-# Preview the data
-print(df.head())
-
-
-video_id = VIDEO_ID[0]
-
-url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id={video_id}&key={api_key}"
-
-response = requests.get(url)
-data = response.json()
-
-# Parse the stats
-if data['items']:
-    stats = data['items'][0]['statistics']
-    details = data['items'][0]['contentDetails']
-
-    views = (stats.get('viewCount'))
-    likes = (stats.get('likeCount'))
-    comments = ( stats.get('commentCount'))
-    isoduration = ( details.get('duration'))
-    duration = isodate.parse_duration(str(isoduration))
-    print(views, likes, comments, duration)
+if os.path.exists(yesterday_csv_path): # check if csv exists, if not create empty df
+    existing_df = pd.read_csv(yesterday_csv_path)
 else:
-    print("Video not found or API key error.")
+    existing_df = pd.DataFrame()
 
+if not existing_df.empty: # if csv exists combine and drop dupes
+    combined_df = pd.concat([existing_df, append_df], ignore_index=True)
+    combined_df.drop_duplicates(subset=['video_id', 'date'], inplace=True)
+else:
+    combined_df = append_df
+
+combined_df.to_csv(today_csv_path, index=False)
