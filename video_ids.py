98fd03c6b8bf41260b4af5ee9ebe25ce097f33a1
Dan <97834906+D-Stokes@users.noreply.github.com>
2025-08-07 18:47:45 +0100

    Update video_ids.py
    New vid, punk soul bass

--- video_ids.py
+++ video_ids.py
@@ -1,3 +1,3 @@
 # file containing the id's of the video I'll be tracking stats for
 
-VIDEO_ID = ["OnUp-0ZDiDQ","5UmZlya3AY8", "-8nWeVXyXNk","X9f7LIhEXN8", "4ervOxAR1xc"]
+VIDEO_ID = ["OnUp-0ZDiDQ","5UmZlya3AY8", "-8nWeVXyXNk","X9f7LIhEXN8", "4ervOxAR1xc","99pGNiKw0-s"]
